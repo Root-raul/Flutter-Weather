6e21efdf77a939ad45c62413cdc43da839043ccb
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-09-15 04:07:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,10 @@ class WeatherApp extends StatelessWidget {
   }
 }
 
+
 /// top level widget to hold application state
 /// state is passed down with an inherited widget
+
 class AppStateContainer extends StatefulWidget {
   final Widget child;
 
